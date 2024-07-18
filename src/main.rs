41dfc17aefac7a6b473def61b9746b8cdbec4bfb
tom <tom@pipefold.io>
2024-07-18 13:33:29 +0100

    xp: collisions

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,101 @@
 use bevy::prelude::*;
-use std::f32::consts::PI;
+use bevy::sprite::MaterialMesh2dBundle;
+
+const RED_SRGB: bevy::prelude::Color = Color::srgb(1., 0., 0.);
+const BLUE_SRGB: bevy::prelude::Color = Color::srgb(0., 0., 1.);
+const GREEN_SRGB: bevy::prelude::Color = Color::srgb(0., 1., 0.);
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
-        .add_systems(Update, update_dot_position)
+        .add_systems(Update, (move_objects, check_collision))
         .run();
 }
 
-// Define a component to mark entities as dots
 #[derive(Component)]
-struct Dot;
+struct MovingObject {
+    center: Vec2,
+    radius: f32,
+    angle: f32,
+    speed: f32,
+}
 
-// Set up the initial scene
-fn setup(mut commands: Commands) {
-    // Spawn a 2D camera
+#[derive(Component)]
+struct Collider {
+    radius: f32,
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
     commands.spawn(Camera2dBundle::default());
 
-    // Spawn a white dot
+    // Spawn two circular objects
     commands.spawn((
-        SpriteBundle {
-            sprite: Sprite {
-                color: Color::WHITE,
-                custom_size: Some(Vec2::new(10.0, 10.0)),
-                ..default()
-            },
+        MaterialMesh2dBundle {
+            mesh: meshes.add(Circle::new(20.0)).into(),
+            material: materials.add(ColorMaterial::from(RED_SRGB)),
+            transform: Transform::from_translation(Vec3::new(-100.0, 0.0, 0.0)),
             ..default()
         },
-        Dot, // Add the Dot component to mark this entity as a dot
+        MovingObject {
+            center: Vec2::new(-100.0, 0.0),
+            radius: 100.0,
+            angle: 0.0,
+            speed: 1.0,
+        },
+        Collider { radius: 20.0 },
+    ));
+
+    commands.spawn((
+        MaterialMesh2dBundle {
+            mesh: meshes.add(Circle::new(20.0)).into(),
+            material: materials.add(ColorMaterial::from(BLUE_SRGB)),
+            transform: Transform::from_translation(Vec3::new(100.0, 0.0, 0.0)),
+            ..default()
+        },
+        MovingObject {
+            center: Vec2::new(100.0, 0.0),
+            radius: 100.0,
+            angle: std::f32::consts::PI,
+            speed: 1.5,
+        },
+        Collider { radius: 20.0 },
     ));
 }
 
-// Update the position of the dot to make it move in a clockwise circle
-fn update_dot_position(time: Res<Time>, mut query: Query<&mut Transform, With<Dot>>) {
-    for mut transform in &mut query {
-        // Calculate the angle based on elapsed time
-        let angle = time.elapsed_seconds() * 2.0 * PI;
-        let radius = 100.0;
+fn move_objects(mut query: Query<(&mut Transform, &mut MovingObject)>, time: Res<Time>) {
+    for (mut transform, mut moving_object) in query.iter_mut() {
+        moving_object.angle += moving_object.speed * time.delta_seconds();
+        let new_position = Vec2::new(
+            moving_object.center.x + moving_object.radius * moving_object.angle.cos(),
+            moving_object.center.y + moving_object.radius * moving_object.angle.sin(),
+        );
+        transform.translation = new_position.extend(0.0);
+    }
+}
 
-        // Update the dot's position
-        // Use negative sine for y to make it move clockwise
-        transform.translation.x = radius * angle.cos();
-        transform.translation.y = -radius * angle.sin(); // Negative sign here for clockwise motion
+fn check_collision(
+    mut query: Query<(&Transform, &Collider, &mut Handle<ColorMaterial>)>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    let mut iter = query.iter_mut();
+    if let Some((transform1, collider1, material1)) = iter.next() {
+        if let Some((transform2, collider2, material2)) = iter.next() {
+            let distance = transform1.translation.distance(transform2.translation);
+            let collision = distance < (collider1.radius + collider2.radius);
+
+            let (color1, color2) = if collision {
+                (GREEN_SRGB, GREEN_SRGB)
+            } else {
+                (RED_SRGB, BLUE_SRGB)
+            };
+
+            *materials.get_mut(material1.id()).unwrap() = ColorMaterial::from(color1);
+            *materials.get_mut(material2.id()).unwrap() = ColorMaterial::from(color2);
+        }
     }
 }
